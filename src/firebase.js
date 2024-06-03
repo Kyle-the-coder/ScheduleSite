583609e6c6a4dc507a6122bfd4227fc807c76c13
Kyle-the-coder <kylethacoder@gmail.com>
2024-06-03 12:15:58 -0700

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKey1 = import.meta.env.VITE_FB_API_KEY;
+const authDomain1 = import.meta.env.VITE_AUTH_DOMAIN;
+const messengerId1 = import.meta.env.VITE_MESSENGER_ID;
+const appId1 = import.meta.env.VITE_APP_ID;
+const measurementId1 = import.meta.env.VITE_MEASUREMENT_ID;
+const projectId1 = import.meta.env.VITE_PROJECT_ID;
+const storageBucket1 = import.meta.env.VITE_STORAGE_BUCKET;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD8jvEVJAQjTP6witn7riBZoqOBAAf1sO0",
-  authDomain: "renewedmobilitysolutions.firebaseapp.com",
-  projectId: "renewedmobilitysolutions",
-  storageBucket: "renewedmobilitysolutions.appspot.com",
-  messagingSenderId: "682157552795",
-  appId: "1:682157552795:web:a7a0f36b65a599c2a4023e",
-  measurementId: "G-8NCQ8FQ1PH",
+  apiKey: apiKey1,
+  authDomain: authDomain1,
+  projectId: messengerId1,
+  storageBucket: appId1,
+  messagingSenderId: measurementId1,
+  appId: projectId1,
+  measurementId: storageBucket1,
 };
 
 // Initialize Firebase
