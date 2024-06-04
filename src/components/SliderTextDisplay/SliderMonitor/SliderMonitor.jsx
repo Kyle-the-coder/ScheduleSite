d8185902326307f2fd1ac1cebad1f7e6b710a66f
Kyle-the-coder <kylethacoder@gmail.com>
2024-06-04 11:01:22 -0700

    updated

---
+++ src/components/SliderTextDisplay/SliderMonitor/SliderMonitor.jsx
@@ -0,0 +1,103 @@
+import gsap from "gsap";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import "./slidermonitor.css";
+import { useNavigate } from "react-router-dom";
+
+export function SliderMonitor({
+  title,
+  paragraph,
+  buttonName,
+  leftOrRight,
+  img,
+  sliderId,
+  isFirstRender,
+  isTopCurve,
+  nav,
+}) {
+  const [navState, setNavState] = useState(nav);
+  const [ref, inView] = useInView({});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isFirstRender) {
+      if (inView) {
+        if (leftOrRight) {
+          gsap.to(`.slider-img-${sliderId}`, {
+            xPercent: 96,
+            duration: 2,
+            delay: 2.5,
+          });
+        } else if (!leftOrRight) {
+          gsap.to(`.slider-img-${sliderId}`, {
+            xPercent: -96,
+            duration: 2,
+            delay: 2.5,
+          });
+        }
+      } else if (!inView) {
+        if (leftOrRight) {
+          gsap.to(`.slider-img-${sliderId}`, {
+            xPercent: 0,
+            duration: 0.5,
+          });
+        } else if (!leftOrRight) {
+          gsap.to(`.slider-img-${sliderId}`, {
+            xPercent: 0,
+            duration: 0.5,
+          });
+        }
+      }
+    }
+  }, [inView]);
+  console.log(nav);
+
+  return (
+    <div
+      className="slider-text-display-container"
+      style={
+        isTopCurve
+          ? { borderTopLeftRadius: "0.5rem", borderTopRightRadius: "0.5rem" }
+          : {}
+      }
+    >
+      {leftOrRight ? (
+        <>
+          <div className="slider-text-display" ref={ref}>
+            {title}
+            <p className="font2 f1-5 m0">{paragraph}</p>
+            <button onClick={() => navigate(nav)} className="slider-button">
+              {buttonName}
+            </button>
+          </div>
+          <div className="slider-img-display">
+            <img
+              src={img}
+              className={`${
+                leftOrRight ? "left" : "right"
+              } slider-img slider-img-${sliderId}`}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="slider-img-display">
+            <img
+              src={img}
+              className={`${
+                leftOrRight ? "left" : "right"
+              } slider-img slider-img-${sliderId}`}
+            />
+          </div>
+          <div className="slider-text-display" ref={ref}>
+            {title}
+            <p className="font2 f1-5">{paragraph}</p>
+            <button onClick={() => navigate(nav)} className="slider-button">
+              {buttonName}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
--- src/pages/LandingPage/LpMonitor/LandingPageMonitor.jsx
+++ src/pages/LandingPage/LpMonitor/LandingPageMonitor.jsx
@@ -2,9 +2,15 @@ import "./landingpagemonitor.css";
 import bgImage from "../../../assets/workout.jpg";
 import top10 from "../../../assets/review.png";
 import quotes from "../../../assets/circle.png";
+import wakeUp from "../../../assets/wakeUp.jpg";
+import experience from "../../../assets/experience.jpg";
 import { useNavigate } from "react-router-dom";
+import { SliderMonitor } from "../../../components/SliderTextDisplay/SliderMonitor/SliderMonitor";
+import { useState } from "react";
+
 export function LandingPageMonitor() {
   const navigate = useNavigate();
+  const [isFirstRender, setIsFirstRender] = useState(false);
 
   function handleBookApptButton() {
     navigate("/schedule");
@@ -45,6 +51,50 @@ export function LandingPageMonitor() {
           <img src={quotes} className="quotes-image" />
         </div>
       </div>
+
+      <SliderMonitor
+        title={
+          <h1 className="font3 f3 m0">
+            <span className="pinkText">H</span>elpful{" "}
+            <span className="greenText">S</span>ervices
+          </h1>
+        }
+        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            fermentum justo eget nisl dapibus, nec venenatis justo malesuada.
+            Curabitur vel libero nec nisi suscipit pharetra. Duis vel risus eu
+            sem fringilla euismod at at tortor. Nullam nec urna in arcu
+            tristique facilisis. Sed fringilla velit at turpis aliquam, nec
+            vestibulum libero ullamcorper."
+        buttonName="Services"
+        leftOrRight={true}
+        isFirstRender={isFirstRender}
+        img={wakeUp}
+        sliderId={1}
+        isTopCurve={true}
+        nav={"/services"}
+      />
+
+      <SliderMonitor
+        title={
+          <h1 className="font3 f2 m0">
+            <span className="pinkText">20+</span> years of{" "}
+            <span className="greenText">E</span>xperience
+          </h1>
+        }
+        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            fermentum justo eget nisl dapibus, nec venenatis justo malesuada.
+            Curabitur vel libero nec nisi suscipit pharetra. Duis vel risus eu
+            sem fringilla euismod at at tortor. Nullam nec urna in arcu
+            tristique facilisis. Sed fringilla velit at turpis aliquam, nec
+            vestibulum libero ullamcorper."
+        buttonName="About Me"
+        leftOrRight={false}
+        isFirstRender={isFirstRender}
+        img={experience}
+        sliderId={2}
+        isTopCurve={true}
+        nav={"/aboutme"}
+      />
     </div>
   );
 }
