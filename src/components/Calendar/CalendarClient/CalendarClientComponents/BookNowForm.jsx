ff5e86a189074d584cf63da135a88c22fd22e4aa
Kyle-the-coder <kylethacoder@gmail.com>
2024-06-17 12:55:13 -0700

    updated

--- src/components/Calendar/CalendarClient/CalendarClientComponents/BookNowForm.jsx
+++ src/components/Calendar/CalendarClient/CalendarClientComponents/BookNowForm.jsx
@@ -1,13 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { format, parse } from "date-fns";
-import close from "../../../../assets/close.png";
-import gsap from "gsap";
 import "../CalendarClientStyles/booknowform.css";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../../../../firebase";
 import emailjs from "emailjs-com";
-import { FormButton } from "../../../FormButton/FormButton";
 import { useBookNowModal } from "../../../context/BookNowModal";
+import closeButton from "../../../../assets/x-button.png";
 
 export function BookNowForm({
   dateOfEvent,
@@ -16,7 +14,8 @@ export function BookNowForm({
   setTimeBlock,
 }) {
   const [error, setError] = useState("");
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [description, setDescription] = useState("");
   const { isBnActive, setIsBnActive } = useBookNowModal();
@@ -42,7 +41,8 @@ export function BookNowForm({
             if (block.id === timeBlock.id) {
               return {
                 ...block,
-                name: name,
+                firstName: firstName,
+                lastName: lastName,
                 email: email,
                 description: description,
                 isAvailableAppt: false,
@@ -71,7 +71,7 @@ export function BookNowForm({
 
           // Send email using EmailJS
           const templateParams = {
-            from_name: name,
+            from_name: firstName - lastName,
             from_email: email,
             from_description: description,
             from_date: reFormattedDate,
@@ -118,53 +118,71 @@ export function BookNowForm({
         <div
           className={`book-now-main-container ${isBnActive ? "active" : ""}`}
         >
-          <div className="book-now-blur-background"></div>
-
           <div className="book-now-container">
-            <div className="book-now-top-container">
-              <h1>Enter Appt. Info</h1>
-              <img
-                src={close}
-                onClick={() => closeModal()}
-                className="book-now-close-button"
-              />
-            </div>
-            <div className="book-now-form-container">
-              <form className="book-now-form" onSubmit={addClientToTimeBlock}>
-                <div className="book-now-form-input-container">
-                  <h2>
-                    Time: {displayStartTime}-{displayEndTime}
-                  </h2>
-                </div>
-                <div className="book-now-form-input-container">
-                  <label className="book-now-text-label">Name:</label>
+            <form className="form">
+              <div className="book-now-close-container">
+                <img
+                  src={closeButton}
+                  width="30px"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => closeModal()}
+                />
+              </div>
+              <p className="title">Book Appointment </p>
+              <div className="book-now-form-input-container">
+                <h2 className="m0">
+                  Time: {displayStartTime}-{displayEndTime}
+                </h2>
+              </div>
+              <p className="message">Those marked with * are required </p>
+              <div className="flex">
+                <label>
                   <input
+                    className="input"
                     type="text"
-                    className="book-now-text-input"
-                    onChange={(e) => setName(e.target.value)}
+                    placeholder=""
+                    required=""
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
-                </div>
-                <div className="book-now-form-input-container">
-                  <label className="book-now-text-label">Email:</label>
+                  <span>Firstname *</span>
+                </label>
+
+                <label>
                   <input
+                    className="input"
                     type="text"
-                    className="book-now-text-input"
-                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder=""
+                    required=""
+                    onChange={(e) => setLastName(e.target.value)}
                   />
-                </div>
-                <div className="book-now-form-area-container">
-                  <label className="book-now-area-label">Description:</label>
-                  <textarea
-                    type="text"
-                    rows="7"
-                    className="book-now-area-input"
-                    onChange={(e) => setDescription(e.target.value)}
-                  />
-                </div>
-                <div className="error-message">{error && error}</div>
-                <FormButton buttonName="Book Appointment" />
-              </form>
-            </div>
+                  <span>Lastname *</span>
+                </label>
+              </div>
+
+              <label>
+                <input
+                  className="input"
+                  type="email"
+                  placeholder=""
+                  required=""
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <span>Email *</span>
+              </label>
+
+              <label>
+                <textarea
+                  className="input"
+                  type="text"
+                  placeholder=""
+                  required=""
+                  rows="7"
+                />
+                <span>Description *</span>
+              </label>
+
+              <button className="submit">Book Appointment</button>
+            </form>
           </div>
         </div>
       )}
