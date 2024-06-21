bb0c1689b3c50eeda85d2203435bc50ad9f7f320
Kyle-the-coder <kylethacoder@gmail.com>
2024-06-21 09:00:17 -0700

    updated

--- src/pages/AboutMePage/AmMonitor/AboutMePageMonitor.jsx
+++ src/pages/AboutMePage/AmMonitor/AboutMePageMonitor.jsx
@@ -1,4 +1,5 @@
 import weights from "../../../assets/weightsBlue.jpg";
+import headShot from "../../../assets/headshot.jpg";
 import BannerMonitor from "../../../components/Banner/BannerMonitor/BannerMonitor";
 import "./aboutmepagemonitor.css";
 export function AboutMePageMonitor() {
@@ -15,7 +16,30 @@ export function AboutMePageMonitor() {
         side={true}
         position="0 47%"
       />
-      <h1>about me page mont</h1>
+      <div className="about-me-info-container">
+        <img src={headShot} className="about-me-headshot" />
+        <div className="about-me-title-container">
+          <h1 className="m0 font2 f3 f-thin">Hello my name is</h1>
+          <h1 className="m0 font3 f3">
+            <span className="darkPinkText">S</span>abrina{" "}
+            <span className="purpleText">M</span>itchell
+          </h1>
+        </div>
+        <div className="about-me-info">
+          <p className="m0 f2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            fermentum justo eget nisl dapibus, nec venenatis justo malesuada.
+            Curabitur vel libero nec nisi suscipit pharetra. Quisque fermentum
+            justo eget nisl dapibus, nec venenatis justo malesuada. Curabitur
+            vel libero nec nisi suscipit pharetra. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Quisque fermentum justo eget nisl
+            dapibus, nec venenatis justo malesuada. Curabitur vel libero nec
+            nisi suscipit pharetra. Quisque fermentum justo eget nisl dapibus,
+            nec venenatis justo malesuada. Curabitur vel libero nec nisi
+            suscipit pharetra.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
